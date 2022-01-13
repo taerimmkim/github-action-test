e9f1511dce65de7f27abeb4a9b36ca0dc5ec0534
taerimmkim <taerimm211@gmail.com>
2022-01-13 22:02:24 +0900

    test

--- dag_validation.py
+++ dag_validation.py
@@ -1,6 +1,7 @@
 from airflow.models import DagBag
+#import pytest
 
 dag_bag = DagBag(dag_folder='./dags/', include_examples=False)
 
 for dag in dag_bag.dags:
-     print(f'"{dag}" DAG is correct!!!')
+    dag.size() 
