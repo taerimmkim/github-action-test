1f89d748c3ac943f7f83d2a12875d695221a7155
taerimmkim <taerimm211@gmail.com>
2022-01-13 22:20:34 +0900

    test2

--- dag_validation.py
+++ dag_validation.py
@@ -3,7 +3,13 @@ import pytest
 
 def test_dag_validation():
     dag_bag = DagBag(dag_folder='./dags/', include_examples=False)
-    assert dag_bag
+    
+    # print(dag_bag.dagbag_report())
+    #for dag in dag_bag.bags:
+        #assert dags.keys() != ''
+    #    print(dag.keys())
+
+test_dag_validation()
 
 #for dag in dag_bag.dags:
 #    dag.size() 
