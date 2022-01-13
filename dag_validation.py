e18e22b8755044ada75ecf7c6776dd8456538d3a
taerimmkim <taerimm211@gmail.com>
2022-01-13 22:00:19 +0900

    remove try-except block

--- dag_validation.py
+++ dag_validation.py
@@ -2,10 +2,5 @@ from airflow.models import DagBag
 
 dag_bag = DagBag(dag_folder='./dags/', include_examples=False)
 
-try:
-  for dag in dag_bag.dags:
-    print(f'"{dag}" DAG is correct!!!')
-except Exceptione as e:
-  print(f'"{dag}" DAG raises "{e}" errors!!!')
-
-
+for dag in dag_bag.dags:
+     print(f'"{dag}" DAG is correct!!!')
