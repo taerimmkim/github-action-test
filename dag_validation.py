94bad678b864987414348548316d5b06df7d436b
taerimmkim <taerimm211@gmail.com>
2022-01-20 00:01:44 +0900

    test4

--- dag_validation.py
+++ dag_validation.py
@@ -4,5 +4,7 @@ import pytest
 def test_dag_validation():
     dag_bag = DagBag(dag_folder='./dags/', include_examples=False)
     expected_dag_size = len(dag_bag.dagbag_stats)
+    print(f"dag_bag size: {dag_bag.size()}")
+    print(f"expected_dag_size: {expected_dag_size}")
     assert dag_bag.size() == expected_dag_size
     
